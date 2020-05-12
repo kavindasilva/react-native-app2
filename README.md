d5e0ae3162b967b91cf489dbe4a65367b80ba0a2
kavindasilva <kss.kavinda@gmail.com>
2020-05-12 20:57:02 +0530

    learning outcomes documented

--- README.md
+++ README.md
@@ -1,2 +1,7 @@
 # react-natvie-app2
 work with react-native-cli
+
+##### Learning outcomes
+in android this error may be thrown: react-native-gesture-handler rngesturehandlermodule.default.direction
+solution: https://github.com/software-mansion/react-native-gesture-handler/issues/676#issuecomment-510131221
+
--- android/app/src/main/java/com/ks2/MainActivity.java
+++ android/app/src/main/java/com/ks2/MainActivity.java
@@ -21,12 +21,12 @@ public class MainActivity extends ReactActivity {
   }
 
   @Override
-    protected ReactActivityDelegate createReactActivityDelegate() {
+  protected ReactActivityDelegate createReactActivityDelegate() {
     return new ReactActivityDelegate(this, getMainComponentName()) {
-    @Override
-    protected ReactRootView createRootView() {
-    return new RNGestureHandlerEnabledRootView(MainActivity.this);
-    }
+      @Override
+      protected ReactRootView createRootView() {
+        return new RNGestureHandlerEnabledRootView(MainActivity.this);
+      }
     };
   }
 }
