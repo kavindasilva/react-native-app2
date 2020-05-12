030de74fe43078a132128ef6541bf26e832c70e4
kavindasilva <kss.kavinda@gmail.com>
2020-05-12 20:00:18 +0530

    fix for rngesturehandlermodule.default.direction

--- android/app/src/main/java/com/ks2/MainActivity.java
+++ android/app/src/main/java/com/ks2/MainActivity.java
@@ -1,6 +1,13 @@
 package com.ks2;
 
+/**
+  in android: react-native-gesture-handler rngesturehandlermodule.default.direction
+  https://github.com/software-mansion/react-native-gesture-handler/issues/676#issuecomment-510131221
+*/
 import com.facebook.react.ReactActivity;
+import com.facebook.react.ReactActivityDelegate;
+import com.facebook.react.ReactRootView;
+import com.swmansion.gesturehandler.react.RNGestureHandlerEnabledRootView;
 
 public class MainActivity extends ReactActivity {
 
@@ -11,5 +18,15 @@ public class MainActivity extends ReactActivity {
   @Override
   protected String getMainComponentName() {
     return "ks2";
+  }
+
+  @Override
+    protected ReactActivityDelegate createReactActivityDelegate() {
+    return new ReactActivityDelegate(this, getMainComponentName()) {
+    @Override
+    protected ReactRootView createRootView() {
+    return new RNGestureHandlerEnabledRootView(MainActivity.this);
+    }
+    };
   }
 }
