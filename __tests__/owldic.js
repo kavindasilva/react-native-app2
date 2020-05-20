9faab17f64807503e06fb210616d57d127e20097
kavindasilva <kss.kavinda@gmail.com>
2020-05-20 20:10:45 +0530

    started unit testing

---
+++ __tests__/owldic.js
@@ -0,0 +1,52 @@
+/**
+ * @format
+ * OwlBot dictionary functions testing
+ */
+
+import 'react-native';
+import React from 'react';
+import App from '../App';
+
+// Note: test renderer must be required after react-native.
+import renderer from 'react-test-renderer';
+
+jest.mock('react-native-gesture-handler', () => {
+  // eslint-disable-next-line global-require
+  const View = require('react-native/Libraries/Components/View/View');
+  return {
+    Swipeable: View,
+    DrawerLayout: View,
+    State: {},
+    ScrollView: View,
+    Slider: View,
+    Switch: View,
+    TextInput: View,
+    ToolbarAndroid: View,
+    ViewPagerAndroid: View,
+    DrawerLayoutAndroid: View,
+    WebView: View,
+    NativeViewGestureHandler: View,
+    TapGestureHandler: View,
+    FlingGestureHandler: View,
+    ForceTouchGestureHandler: View,
+    LongPressGestureHandler: View,
+    PanGestureHandler: View,
+    PinchGestureHandler: View,
+    RotationGestureHandler: View,
+    /* Buttons */
+    RawButton: View,
+    BaseButton: View,
+    RectButton: View,
+    BorderlessButton: View,
+    /* Other */
+    FlatList: View,
+    gestureHandlerRootHOC: jest.fn(),
+    Directions: {},
+  };
+});
+jest.mock('react-native-reanimated', () => require('react-native-reanimated/mock') );
+
+
+// it('renders correctly', () => {
+//   renderer.create(<App />);
+// });
